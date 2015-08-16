ea585d1522b33d7c5396cb0d2c6c07405495ad48
Andrew Cull <andrew.cull@gmail.com>
2015-08-16 17:09:51 -0500

    Adding packs class to deal with number of cards and probabilities and eventually simulate openings

---
+++ hs_packs/Packs.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace hs_packs
+{
+	public class Packs
+	{
+		public float common;
+		public float rare;
+		public float epic;
+		public float legendary;
+		public float gold;
+		
+
+		public Packs ()
+		{
+			Prob cardProb = new Prob ();
+			common = 50.0;
+			rare = 9;
+			epic = 9;
+			legendary = 1.01;
+			gold = 5;
+		}
+
+
+	}
+}
+
